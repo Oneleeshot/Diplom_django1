894aebda0e3e1ff80cb92cc99394b32fc638ea6e
Oneleeshot <instal9595@gmail.com>
2021-02-22 16:07:04 +0300

    Reform views

--- Diplom/shop/views.py
+++ Diplom/shop/views.py
@@ -9,7 +9,7 @@ from cart.forms import CartAddProductForm
 class GlassesView(ListView):
     model = Product
     template_name = 'shop/glasses.html'
-    context_object_name = "products"
+    context_object_name = 'products'
 
     def get(self, request, *args, **kwargs):
         search = request.GET.get('search', '')
@@ -17,10 +17,40 @@ class GlassesView(ListView):
             products = Product.objects.filter(
                 Q(name__icontains=search) or Q(
                     category__icontains=search))
+            return render(request, 'shop/glasses.html',
+                          context={'products': products})
         else:
             products = Product.objects.all()
+            if kwargs != {}:
+                if len(kwargs) == 2:
+                    product_category = kwargs['category']
+                    product_type = kwargs['type'].title()
+                    products = Product.objects.filter(type=product_type,
+                                                      category=product_category)
+                    return render(request, 'shop/glasses.html',
+                                  context={'products': products})
+                else:
+                    product_type = kwargs['type'].title()
+                    products = Product.objects.filter(type=product_type)
+                    return render(request, 'shop/glasses.html',
+                                  context={'products': products})
+            else:
+                return render(request, 'shop/glasses.html',
+                              context={'products': products})
 
-        paginator = Paginator(products, 6)
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product,
+                                slug=slug,
+                                available=True)
+    cart_product_form = CartAddProductForm()
+    return render(request, 'shop/product_detail.html',
+                  {'product': product, 'cart_product_form': cart_product_form})
+
+
+"""Наброски пагинации"""
+"""
+paginator = Paginator(products, 6)
         page_number = request.GET.get('page', 1)
         page = paginator.get_page(page_number)
         is_paginator = page.has_other_pages()
@@ -41,56 +71,4 @@ class GlassesView(ListView):
             'prev_url': prev_url,
             'products': products,
         }
-        return render(request, 'shop/glasses.html',
-                      context=context)
-
-
-class SunGlassesView(ListView):
-    model = Product
-    template_name = 'shop/sun_glasses.html'
-    queryset = Product.objects.filter(type='Sun')
-    context_object_name = 'products'
-
-
-class SunMenGlassesView(ListView):
-    model = Product
-    template_name = 'shop/sun_glasses.html'
-    context_object_name = "products"
-    queryset = Product.objects.filter(type='Sun', category=1)
-
-
-class SunWomenGlassesView(ListView):
-    model = Product
-    template_name = 'shop/sun_glasses.html'
-    context_object_name = "products"
-    queryset = Product.objects.filter(type='Sun', category=2)
-
-
-class OpticalGlassesView(ListView):
-    model = Product
-    template_name = 'shop/optical_glasses.html'
-    context_object_name = "products"
-    queryset = Product.objects.filter(type='Optical')
-
-
-class OpticalManGlassesView(ListView):
-    model = Product
-    template_name = 'shop/optical_glasses.html'
-    context_object_name = "products"
-    queryset = Product.objects.filter(type='Optical', category=1)
-
-
-class OpticalWomenGlassesView(ListView):
-    model = Product
-    template_name = 'shop/optical_glasses.html'
-    context_object_name = "products"
-    queryset = Product.objects.filter(type='Optical', category=2)
-
-
-def product_detail(request, slug):
-    product = get_object_or_404(Product,
-                                slug=slug,
-                                available=True)
-    cart_product_form = CartAddProductForm()
-    return render(request, 'shop/product_detail.html',
-                  {'product': product, 'cart_product_form': cart_product_form})
+"""
